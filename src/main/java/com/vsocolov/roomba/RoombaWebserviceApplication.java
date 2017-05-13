6cc0eb1f093fa14ac832477d7cb4a303e3ae3a6f
Vladislav Socolov <socolov.vladislav@gmail.com>
2017-05-13 13:03:22 +0300

    Added base project skeleton

---
+++ src/main/java/com/vsocolov/roomba/RoombaWebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.vsocolov.roomba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombaWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoombaWebserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vsocolov/roomba/RoombaWebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vsocolov.roomba;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoombaWebserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
